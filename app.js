b0dd04ef0cbbeaaaaa71ee58914c54e01ddb8a41
谭华 <77256361+tanhuagt@users.noreply.github.com>
2022-02-17 14:30:46 +0800

    remove Invalid import

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
@@ -12,10 +10,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
